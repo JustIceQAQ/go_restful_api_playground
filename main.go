0ea682c096c928a1a3f4c2a9884107ed546e23d5
justiceqaq <justiceqaq@gmail.com>
2022-06-19 19:49:43 +0800

    tweak: main usage

--- main.go
+++ main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"fmt"
+	handler "go_restful_api_playground/handler"
+	models "go_restful_api_playground/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func main() {
-	r := setupRouter()
+
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	migrate(db)
+	handlers := handler.NewHandler(db)
+	DB = db
+	r := setupRouter(handlers)
 	setting(r)
+
+	_, _ = models.User.Insert(models.User{}, "admin", "admin", "admin", DB)
+	_, _ = models.User.Insert(models.User{}, "admin2", "admin2", "admin2", DB)
 
 	// Runner
 	if err := r.Run(); err != nil {
