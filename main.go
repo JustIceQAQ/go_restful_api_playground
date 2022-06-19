45d2c81cac7337eb2d48562a2818b53b2ba06f04
justiceqaq <justiceqaq@gmail.com>
2022-06-20 00:16:54 +0800

    tweak: main usage

--- main.go
+++ main.go
@@ -4,9 +4,18 @@ import (
 	"fmt"
 	handler "go_restful_api_playground/handler"
 	models "go_restful_api_playground/models"
+	utils "go_restful_api_playground/utils"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
+
+// Swagger
+// @title Demo GO Restful API
+// @version 0.0.1
+// @BasePath /api/v1
+// @securityDefinitions.apikey BearerAuth
+// @in header
+// @name Authorization
 
 var DB *gorm.DB
 
@@ -22,11 +31,20 @@ func main() {
 	r := setupRouter(handlers)
 	setting(r)
 
-	_, _ = models.User.Insert(models.User{}, "admin", "admin", "admin", DB)
-	_, _ = models.User.Insert(models.User{}, "admin2", "admin2", "admin2", DB)
+	// Demo Process
+	DemoProcess()
 
 	// Runner
 	if err := r.Run(); err != nil {
 		fmt.Printf("startup service failed, err:%v\n\n", err)
 	}
+}
+
+func DemoProcess() {
+	demoUser1Password, _ := utils.HashingPassword("admin")
+
+	_, _ = models.User.Insert(models.User{}, "admin", demoUser1Password, "admin", DB)
+
+	demoUser2Password, _ := utils.HashingPassword("admin2")
+	_, _ = models.User.Insert(models.User{}, "admin2", demoUser2Password, "admin2", DB)
 }
