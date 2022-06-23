92d8ca0fd880037b461714b8283f284f8db9a8b4
justiceqaq <justiceqaq@gmail.com>
2022-06-23 23:38:05 +0800

    refactor: configs.go usage

--- configs/configs.go
+++ configs/configs.go
@@ -10,8 +10,20 @@ import (
 var Cfg = config{}
 
 type config struct {
+	DefaultConfig
+	Cloudinary
+}
+type Cloudinary struct {
+	CloudName    string `env:"CLOUDINARY_CLOUD_NAME"`
+	ApiKey       string `env:"CLOUDINARY_API_KEY"`
+	ApiSecret    string `env:"CLOUDINARY_API_SECRET"`
+	UploadFolder string `env:"CLOUDINARY_UPLOAD_FOLDER"`
+}
+
+type DefaultConfig struct {
 	GinMode     string `env:"GIN_MODE" envDefault:"debug"`
 	DatabaseUrl string `env:"DATABASE_URL"`
+	UploadTo    string `env:"UPLOAD_TO"`
 }
 
 func (c *config) show() {
@@ -29,6 +41,7 @@ func init() {
 		}
 
 		Cfg.DatabaseUrl = os.Getenv("DATABASE_URL")
+		Cfg.UploadTo = os.Getenv("UPLOAD_TO")
 	}
 
 	Cfg.show()
